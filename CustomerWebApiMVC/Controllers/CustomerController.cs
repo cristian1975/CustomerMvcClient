a615fc695dbc5b4ee08f4a88c1f4eb18b7752fe0
Red <Red@REDMAN-PC>
2018-11-11 12:54:46 +0200

    Add project files.

---
+++ CustomerWebApiMVC/Controllers/CustomerController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using CustomerWebApiMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CustomerWebApiMVC.Controllers
+{
+    public class CustomerController : Controller
+    {
+        public ActionResult Index()
+        {
+            IEnumerable<Customer> custList;
+            HttpResponseMessage response = GlobalVariables.WebApiClient.GetAsync("Customers").Result;
+            custList = response.Content.ReadAsAsync<IEnumerable<Customer>>().Result;
+            return View(custList);
+        }
+
+        public ActionResult AddOrEdit(string id = "")
+        {
+            if (string.IsNullOrEmpty(id))
+                return View(new Customer());
+            else
+            {
+                HttpResponseMessage response = GlobalVariables.WebApiClient.GetAsync("Customers/" + id.ToString()).Result;
+                return View(response.Content.ReadAsAsync<Customer>().Result);
+            }
+        }
+        [HttpPost]
+        public ActionResult AddOrEdit(Customer cust)
+        {
+            if (string.IsNullOrEmpty(cust.Id))
+            {
+                HttpResponseMessage response = GlobalVariables.WebApiClient.PostAsJsonAsync("Customers", cust).Result;
+                TempData["SuccessMessage"] = "Saved Successfully";
+            }
+            else
+            {
+                HttpResponseMessage response =  GlobalVariables.WebApiClient.PutAsJsonAsync("Customers/" + cust.Id, cust).Result;
+                TempData["SuccessMessage"] = "Updated Successfully";
+            }
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Delete(string id)
+        {
+            HttpResponseMessage response = GlobalVariables.WebApiClient.DeleteAsync("Customers/" + id).Result;
+            TempData["SuccessMessage"] = "Deleted Successfully";
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ CustomerWebApiMVC/GlobalVariables.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace CustomerWebApiMVC
+{
+    public static class GlobalVariables
+    {
+        public static HttpClient WebApiClient = new HttpClient();
+
+        static GlobalVariables()
+        {
+            WebApiClient.BaseAddress = new Uri("https://customer-web-api.herokuapp.com/api/");
+            WebApiClient.DefaultRequestHeaders.Clear();
+            WebApiClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+        }
+    }
+}
