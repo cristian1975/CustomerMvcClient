a0037c466ed9ddf638b7981587d8cb160cd67543
Red <Red@REDMAN-PC>
2018-11-12 20:05:16 +0200

    - add browser directory

--- CustomerWebApiMVC/Startup.cs
+++ CustomerWebApiMVC/Startup.cs
@@ -32,6 +32,7 @@ namespace CustomerWebApiMVC
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddDirectoryBrowser();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
